e2bda96ce8ea263a2b3876973a1ae4b1ad613bc9
Rohan Bhise <Rohan.Bhise@asl.mum.sst>
2023-07-08 12:35:07 +0530

    v1

---
+++ src/com/aurionpro/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.aurionpro.test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hi this is rohan bhise");
+		System.out.println("Branch : ExTC");
+		System.out.println("College : Terna College of Engineering");
+	} 
+}
